6996305814890ec2052445fdba4abae4ba18fa33
Carlos <cjimenez9811@gmail.com>
2023-03-12 16:38:31 -0700

    Creating and Labeling Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//Grabbing the element named root from the DOM, so that we can render our program
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
+  //This is where the rendering will happen 
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
